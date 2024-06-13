2cce171339078e84f3d6ce518b1428cb6f0c7199
guifers <guifers23@gmail.com>
2024-06-13 20:35:35 +0200

    Ficheros base 2016

---
+++ SleepStation/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace SleepStation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton2 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.materialRaisedButton3 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.ForeColor = System.Drawing.SystemColors.Control;
+            this.materialFlatButton1.Location = new System.Drawing.Point(33, 608);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(6, 9, 6, 9);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(65, 36);
+            this.materialFlatButton1.TabIndex = 0;
+            this.materialFlatButton1.Text = "Play";
+            this.materialFlatButton1.UseVisualStyleBackColor = false;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 726);
+            this.materialLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(151, 27);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "Time To Sleep";
+            this.materialLabel1.Click += new System.EventHandler(this.materialLabel1_Click);
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(225, 726);
+            this.materialLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(43, 27);
+            this.materialLabel2.TabIndex = 2;
+            this.materialLabel2.Text = "gfz";
+            this.materialLabel2.Click += new System.EventHandler(this.materialLabel2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(18, 18);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(364, 304);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 449);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(33, 512);
+            this.materialRaisedButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(165, 43);
+            this.materialRaisedButton1.TabIndex = 5;
+            this.materialRaisedButton1.Text = "SET TIME";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // materialRaisedButton2
+            // 
+            this.materialRaisedButton2.Depth = 0;
+            this.materialRaisedButton2.Location = new System.Drawing.Point(51, 334);
+            this.materialRaisedButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.materialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton2.Name = "materialRaisedButton2";
+            this.materialRaisedButton2.Primary = true;
+            this.materialRaisedButton2.Size = new System.Drawing.Size(147, 35);
+            this.materialRaisedButton2.TabIndex = 6;
+            this.materialRaisedButton2.Text = "check";
+            this.materialRaisedButton2.UseVisualStyleBackColor = true;
+            this.materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.BackColor = System.Drawing.SystemColors.Control;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.materialFlatButton2.Location = new System.Drawing.Point(160, 608);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(6, 9, 6, 9);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(66, 36);
+            this.materialFlatButton2.TabIndex = 7;
+            this.materialFlatButton2.Text = "STOP";
+            this.materialFlatButton2.UseVisualStyleBackColor = false;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 558);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 9;
+            // 
+            // materialRaisedButton3
+            // 
+            this.materialRaisedButton3.Depth = 0;
+            this.materialRaisedButton3.Location = new System.Drawing.Point(380, 18);
+            this.materialRaisedButton3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.materialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton3.Name = "materialRaisedButton3";
+            this.materialRaisedButton3.Primary = true;
+            this.materialRaisedButton3.Size = new System.Drawing.Size(70, 34);
+            this.materialRaisedButton3.TabIndex = 11;
+            this.materialRaisedButton3.Text = "HELP";
+            this.materialRaisedButton3.UseVisualStyleBackColor = true;
+            this.materialRaisedButton3.Click += new System.EventHandler(this.materialRaisedButton3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 60000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(531, 775);
+            this.Controls.Add(this.materialRaisedButton3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.materialRaisedButton2);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Sleep Station";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private System.Windows.Forms.Label label1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ SleepStation/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using WMPLib;
+
+namespace SleepStation //Para la proxima version mostrar la cancion que se esta reproduciendo actualmente
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer wmsound = new WMPLib.WindowsMediaPlayer();
+
+        
+        string[] archivos;
+        private int reloj=1000;
+        private decimal minutos = 0;
+        int mp3 = 0;
+        string choose;
+        string realg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+                    
+
+            //object escogido = Path.GetFileName(listBox1.SelectedItem.ToString()); //Me la saque aqui           
+           // materialLabel3.Text = escogido.ToString();
+               
+            //wmsound.URL = listBox1.SelectedItem.ToString();
+            //wmsound.controls.play();
+            //MessageBox.Show(archivos.Length.ToString());
+            //MessageBox.Show(mp3.ToString());
+            /*WMPLib.IWMPPlaylist playlist = wmsound.playlistCollection.newPlaylist("myplaylist");
+            WMPLib.IWMPMedia media;*/
+            try
+            {
+                choose = Path.GetFullPath(listBox1.SelectedItem.ToString()); //Es necesaria la ruta entera, no vale con un Path.GetFilename
+                realg = Convert.ToString(choose);
+            }catch(Exception ex)
+            {
+                MessageBox.Show("ERROR : " + ex.Message.ToString()+ "You selected a song??", "SleepStation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+            /*if (realg.Length > 0) //Compruebo si ha seleccionado alguna cancion para reproducirla antes de continuar con la playlist
+            {
+                
+                    //MessageBox.Show(choose);
+                    media = wmsound.newMedia(realg);
+                    playlist.appendItem(media);
+                    wmsound.currentPlaylist = playlist;
+                    wmsound.controls.play();
+                
+                
+            }
+            */
+            //Ahora tiramos con la playlist
+            WMPLib.IWMPPlaylist playlist2 = wmsound.playlistCollection.newPlaylist("myplaylist2");
+            WMPLib.IWMPMedia media2;
+            try
+            {
+                string first = realg; //Aqui va la primera en caso de que haya escogido una (solucion final)
+                media2 = wmsound.newMedia(first);
+                playlist2.appendItem(media2);
+
+                foreach (string name in archivos)
+                {
+
+
+
+                    //wmsound.URL = name;
+                    //wmsound.controls.play();
+
+                    string extension = Path.GetExtension(name); //De nuevo filtramos solo los .mp3
+                    if (extension == ".mp3")
+                    {
+                        //MessageBox.Show(name);
+                        //Y añadimos cancion a cancion del directorio escogido
+                        media2 = wmsound.newMedia(name);
+                        playlist2.appendItem(media2);
+                        //var playlist = wmsound.newPlaylist("My playlist", "");
+                        //playlist.appendItem(wmsound.newMedia(name));
+                        //wmsound.currentPlaylist = playlist;
+                        //wmsound.controls.play();
+                    }
+
+
+
+
+                }
+                //Arrancamos la lista
+                wmsound.currentPlaylist = playlist2;
+                wmsound.controls.play();
+                
+                timer1.Enabled = true;
+                timer1.Start();
+                timer3.Enabled = true;
+                timer3.Start();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR TO LOAD AND PLAY THE TRACK : " + ex.Message.ToString() + "PRESS THE HELP BUTTON IF YOU NEED IT", "SleepStation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+                
+           
+          
+
+
+            //MessageBox.Show(wmsound.currentMedia.name);
+
+          
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            reloj--; //el contador de apagado
+            if (reloj <= 0)
+            {
+                Process.Start("shutdown", "/s /t 0"); //Tira de system para arrancar 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (textBox1.Text.Length == 0)
+                {
+                    MessageBox.Show("SET A VALID TIME!", "SleepStation" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                if (timer1.Enabled == true)
+                {
+                    MessageBox.Show("STOP THE COUNTER FIRST", "SleepStation" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    reloj = Convert.ToInt32(textBox1.Text);
+                    minutos = reloj / 60;
+                    materialLabel2.Text = minutos.ToString() + " " + "min to sleep"; //Muchas conversiones?¿
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "SleepStation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            wmsound.controls.stop();
+            timer1.Stop();
+            timer1.Enabled = false;
+            timer3.Enabled = false;
+            
+        }
+
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var carpetas = new FolderBrowserDialog();
+                DialogResult result = carpetas.ShowDialog();
+                archivos = Directory.GetFiles(carpetas.SelectedPath);
+                foreach (string name in archivos)
+                {
+                    string extension = Path.GetExtension(name);
+                    if (extension == ".mp3")
+                    {
+                        listBox1.Items.Add(name);
+                        mp3++;
+                    }
+
+                }
+                //listBox1.Sorted = true;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error al tratar la ruta : "+ex.Message.ToString(),"SleepStation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialRaisedButton3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("- Set the time on SECONDS\n" +
+                "- Time to off appears in MINUTES\n"+"- Only files with .mp3 extension will appear in the check moment\n"+"\n" + "-By sad0g--January 2016","Sleep Station HELP",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void materialLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //materialLabel4.Text = wmsound.controls.currentItem.duration.ToString();
+        }
+
+        private void materialLabel1_Click(object sender, EventArgs e)
+        {
+            //Hacer progressbar
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            minutos--;
+            materialLabel2.Text = minutos.ToString() + " " + "min to sleep"; //Pa la proxima dejar escoger min o sec a mostrar
+
+        }
+
+        private void materialLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
